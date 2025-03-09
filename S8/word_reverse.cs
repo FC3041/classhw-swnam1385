6bda2deb369561e614c75bdaa7192cac8bca939c
Sanam <swnam>
2025-03-09 22:16:23 +0330

    string reverse

---
+++ S8/word_reverse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class ReverseWords 
+{
+	public static void Main()
+	{
+		string[] s;
+        s = "in string ro bargardun".Split(' ');
+		string rev = "";
+		for (int i = s.Length - 1;i >= 0; i--) 
+		{
+			rev += s[i] + " ";
+		}
+		Console.Write(rev.Substring(0,rev.Length - 1));
+	}
+}
+
