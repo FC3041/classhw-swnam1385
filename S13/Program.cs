fce52c06207fb8091f7c58358f44e81270a61c0d
Sanam <swnam>
2025-04-12 16:18:04 +0330

    S13

---
+++ S13/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace s13con;
+
+class Program
+{
+    // public static void PrintPerson(Student s)
+    // {
+    //     Console.WriteLine(s.FullName);
+    // }
+
+    // public static void PrintPerson(Teacher s)
+    // {
+    //     Console.WriteLine(s.FullName);
+    // }    
+    public static void PrintPersons(IPerson<int>[] ps)
+    {
+        foreach(var p in ps)
+            Console.WriteLine(p);
+    }    
+    public static void Main(string[] args)
+    {
+        Student [] students = new Student[] { 
+            new Student() {
+                FirstName="Nima",
+                LastName="Hamedi",
+                GPA = 17.23,
+                Id = 21
+            },
+            new Student() {
+                FirstName="Maryam",
+                LastName="Kashani",
+                GPA = 19.5,
+                Id = 23
+            },
+            new Student() {
+                FirstName="Zhaleh",
+                LastName="Aras",
+                GPA = 18.11,
+                Id = 19
+            },
+            new Student() {
+                FirstName="Seda",
+                LastName="Rahamni",
+                GPA = 19,
+                Id = 13
+            },
+        };
+
+        PrintPersons(students);
+
+        MySort(students, PersonComparers.PersonFirstNameComparer);
+        System.Console.WriteLine("----------------");
+        PrintPersons(students);
+
+        
+
+        MySort(students, new PersonLastNameComparer());
+        System.Console.WriteLine("----------------");
+        PrintPersons(students);
+
+        // Teacher t = new Teacher() {
+        //     FirstName = "Zari",
+        //     LastName="Mohasebi"
+        // };
+        // PrintPerson(s);
+        // PrintPerson(t);
+
+    }
+
+    private static void MySort(IPerson<int>[] persons, IComparer<IPerson<int>> cmp)
+    {
+        for(int i=0; i<persons.Length; i++)
+            for(int j=i+1; j<persons.Length;j++)
+                if (cmp.Compare(persons[i], persons[j]) > 0)
+                    swap<IPerson<int>>(persons, i, j);
+    }
+
+    private static void swap<T>(T[] items, int i, int j)
+    {
+        T tmp = items[i];
+        items[i] = items[j];
+        items[j] = tmp;
+    }
+
+    #region Hide
+    static void Main2(string[] args)
+    {
+        try
+        {
+            int x = 0;
+            int w = 5 / x;
+
+            // code after potential error
+        }
+        catch(FormatException e)
+        {
+            // code error handling
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        catch(NullReferenceException nfe)
+        {
+
+        }
+        finally
+        {
+
+        }
+    }
+    #endregion
+}
---
+++ S13/Student.cs
@@ -0,0 +1,99 @@
+
+interface IPerson<_Type>: IComparable<IPerson<_Type>>
+{
+    _Type Id {get; set;}
+    string FirstName {get; set;}
+    string LastName {get; set;}
+
+    string FullName {get;}
+}
+
+
+
+
+class PersonComparers
+{
+    // TODO3 include all comparers in this singleton class.
+    
+    public static PersonFirstNameComparer PersonFirstNameComparer = new ();
+    public static PersonIdComparer PersonIdComparer = new();
+    public static PersonFullNameComparer PersonFullNameComparer = new();
+    public static PersonLastNameComparer PersonLastNameComparer = new();
+}
+
+
+
+// TODO1 implementPersonIdComparer
+class PersonIdComparer : IComparer<IPerson<int>>
+{
+    public int Compare(IPerson<int> x, IPerson<int> y)
+    {
+        return x.Id.CompareTo(y.Id);
+    }
+}
+
+
+
+
+
+// TODO2 implementPersonFullNameComparer
+class PersonFullNameComparer : IComparer<IPerson<int>>
+{
+    public int Compare(IPerson<int> x, IPerson<int> y)
+    {
+        return x.FullName.CompareTo(y.FullName);
+    }
+}
+
+
+
+
+class PersonFirstNameComparer : IComparer<IPerson<int>>
+{
+    public int Compare(IPerson<int> x, IPerson<int> y)
+    {
+        return x.FirstName.CompareTo(y.FirstName);
+    }
+}
+
+
+
+class PersonLastNameComparer : IComparer<IPerson<int>>
+{
+    public int Compare(IPerson<int> x, IPerson<int> y)
+    {
+        return x.LastName.CompareTo(y.LastName);
+    }
+}
+
+
+
+class Student: IPerson<int>
+{
+    public string FirstName {get; set;}
+    public string LastName { get; set;}
+    public string FullName => FirstName + " " + LastName;
+
+    public double GPA {get; set;}
+    public int Id { get; set; }
+
+    // public int CompareTo(Student other)
+
+    public int CompareTo(IPerson<int> other)  => FirstName.CompareTo(other.FirstName);
+
+    public override string ToString() => $"{FullName}\t{GPA}\t{Id}";
+}
+class Teacher: IPerson<string>
+{
+    public string FirstName {get; set;}
+    public string LastName { get; set;}
+    public string FullName => FirstName + LastName;
+    public double Rating {get; set;}
+    public string Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public int CompareTo(IPerson<string> other)
+    {
+        throw new NotImplementedException();
+    }
+}
+
